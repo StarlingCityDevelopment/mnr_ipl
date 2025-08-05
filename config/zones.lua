7a37d312fd75314ac8e71abf6eb13f43888a645c
Codexis <contact@codexis.dev>
2025-08-05 16:57:20 +0200

    refactor: configuration

--- config/zones.lua
+++ config/zones.lua
@@ -1,3 +1,38 @@
 return {
-
+    ["mile-high-hotel-1"] = {
+        coords = vec4(-147.78, -986.99, 181.30, 160.00),
+        size = vec3(22.0, 60.0, 2.5),
+        debug = false,
+        ipl = "mile-high-hotel-1",
+    },
+    ["mile-high-hotel-2"] = {
+        coords = vec4(-147.78, -986.99, 187.70, 160.00),
+        size = vec3(22.0, 60.0, 2.5),
+        debug = false,
+        ipl = "mile-high-hotel-2",
+    },
+    ["mile-high-hotel-3"] = {
+        coords = vec4(-147.78, -986.99, 193.90, 160.00),
+        size = vec3(22.0, 60.0, 2.5),
+        debug = false,
+        ipl = "mile-high-hotel-3",
+    },
+    ["mile-high-hotel-4"] = {
+        coords = vec4(-147.78, -986.99, 200.00, 160.00),
+        size = vec3(22.0, 60.0, 2.5),
+        debug = false,
+        ipl = "mile-high-hotel-4",
+    },
+    ["mile-high-hotel-5"] = {
+        coords = vec4(-147.78, -986.99, 206.40, 160.00),
+        size = vec3(22.0, 60.0, 2.5),
+        debug = false,
+        ipl = "mile-high-hotel-5",
+    },
+    ["mile-high-hotel-6"] = {
+        coords = vec4(-147.78, -986.99, 212.60, 160.00),
+        size = vec3(22.0, 60.0, 2.5),
+        debug = false,
+        ipl = "mile-high-hotel-6",
+    },
 }
