4dab4547ec2f5fabf86520266afd1bd379585ba2
IlMelons <melonss2002@gmail.com>
2025-09-20 11:13:11 +0200

    Added comments and coords

--- config/disable.lua
+++ config/disable.lua
@@ -1,25 +1,28 @@
 return {
-    -- Ammu-Nation IDs
-    { id = 137729, disable = false },
-    { id = 248065, disable = false },
-    { id = 153857, disable = false },
-    { id = 140289, disable = false },
-    { id = 164609, disable = false },
-    { id = 168193, disable = false },
-    { id = 176385, disable = false },
-    { id = 178689, disable = false },
-    { id = 200961, disable = false },
-    { id = 180481, disable = false },
-    { id = 175617, disable = false },
-    -- Fleeca Bank IDs
+    ---@description Ammu-Nation Interiors
+    { id = 137729, disable = false },               --- Los Santos      | Adam's Apple Boulevard    | coords: 16.5983, -1117.1483, 29.7913
+    { id = 248065, disable = false },               --- Los Santos      | Popular Street            | coords: 811.9525, -2145.9695, 29.3703
+    { id = 153857, disable = false },               --- Los Santos      | Vespucci Boulevard        | coords: 843.8707, -1021.2626, 27.5487
+    { id = 140289, disable = false },               --- Los Santos      | Spanish Avenue            | coords: 243.1246, -45.0122, 69.8966
+    { id = 164609, disable = false },               --- Los Santos      | Boulevard Del Perro       | coords: -1315.4467, -390.1280, 36.5377
+    { id = 168193, disable = false },               --- Los Santos      | Lindsay Circus            | coords: -663.7717, -947.1395, 21.5986
+    { id = 176385, disable = false },               --- Los Santos      | Great Ocean Highway       | coords: -3163.2073, 1082.1978, 20.8478
+    { id = 178689, disable = false },               --- Los Santos      | Palomino Freeway          | coords: 2569.5364, 305.0328, 108.6081
+    { id = 200961, disable = false },               --- Sandy Shores    | Algonquin Boulevard       | coords: 1699.7914, 3750.7307, 34.3718
+    { id = 175617, disable = false },               --- Sandy Shores    | Route 68                  | coords: -1111.4142, 2688.8916, 18.5997
+    { id = 180481, disable = false },               --- Paleto Bay      | Great Ocean Highway       | coords: -323.0224, 6073.8945, 31.2306
+    
+    ---@description Fleeca Bank Interiors
+    { id = 137985, disable = false },               --- Los Santos      | Vespucci Boulevard        | coords: 151.3034, -1036.2970, 29.3392
     { id = 234241, disable = false },
     { id = 141057, disable = false },
-    { id = 137985, disable = false },
     { id = 165121, disable = false },
     { id = 174849, disable = false },
-    { id = 201985, disable = false },
-    -- Pillbox Hospital
+    { id = 201985, disable = false },               --- Sandy Shores    | Route 68                  | coords: 1175.1665, 2701.2539, 38.1727
+    
+    ---@description Pillbox Hospital IPL
     { ipl = 'rc12b_default', disable = false },
-    -- Trevor Trailer
+    
+    ---@description Trevor Trailer Interior
     { id = 2562, disable = false },
 }
