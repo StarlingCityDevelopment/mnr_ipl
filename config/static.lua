eb08aff8e4d161d8c5abbd4c3cbc0b8ed861f492
IlMelons <melonss2002@gmail.com>
2025-10-09 15:20:03 +0200

    Removed Placeholder

--- config/static.lua
+++ config/static.lua
@@ -1,8 +1,3 @@
 return {
-    -- NEW STATIC CUSTOM MAP IPL PLACEHOLDER (Useful for maps that need changeable ipls)
-    -- ['example_map_name'] = {
-    --     ['name1'] = { ipl = 'ipl_name_', default = true },
-    --     ['name2'] = { ipl = 'ipl_name_', default = true },
-    --     ['name3'] = { ipl = 'ipl_name_', default = true },
-    -- },
+
 }
